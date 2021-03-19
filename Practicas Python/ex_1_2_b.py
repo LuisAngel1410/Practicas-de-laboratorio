d672fb16419d951b034ff284233660783f509a5a
LuisAngel1410 <79875867+LuisAngel1410@users.noreply.github.com>
2021-03-18 21:19:44 -0600

    R.P.1

--- Practicas Python/ex_1_2_b.py
+++
@@ -1,5 +0,0 @@
-print('Suma de dos reales')
-a = float( input('Entre el dato 1: ') )
-b = float( input('Entre el dato 2: ') )
-suma = a + b
-print('La suma vale:' , suma)
--- Practicas Python/ex_2_3.py
+++
@@ -1,8 +0,0 @@
-#Puntos Bidimencionales
-ax , cy= int (input('Ingrese el valor de ax:\n'))
-ay = int (input('Ingrese el valor de ay:\n'))
-bx = int (input('Ingrese el valor de bx:\n'))
-by = int (input('Ingrese el valor de by:\n'))
-#Formula
-m = (ax + bx)/2, (ay + by)/2
-print('Los puntos medios son:\n',m)
